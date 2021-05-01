aeb40e88d8c8396e6aaf546319b2d260c9d4e0cc
Morgan Bailey <mabailey079@Gmail.com>
2021-05-01 10:55:35 -0400

    adding offline functionality

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,12 +3,12 @@ const FILES_TO_CACHE = [
   "/index.html",
   "/styles.css",
   "/index.js",
-  "./icons/icon-192x192.png",
-  "./icons/icon-512x512.png",
+  "/db.js",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
   "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
   "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
 ];
-
 const PRECACHE = "precache-v1";
 const RUNTIME = "runtime";
 
@@ -16,49 +16,62 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     caches
       .open(PRECACHE)
-      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then((cache) => {
+        console.log("precache successful!");
+        return cache.addAll(FILES_TO_CACHE);
+      })
       .then(self.skipWaiting())
   );
 });
 
 // The activate handler takes care of cleaning up old caches.
 self.addEventListener("activate", (event) => {
-  const currentCaches = [PRECACHE, RUNTIME];
   event.waitUntil(
-    caches
-      .keys()
-      .then((cacheNames) => {
-        return cacheNames.filter(
-          (cacheName) => !currentCaches.includes(cacheName)
-        );
-      })
-      .then((cachesToDelete) => {
-        return Promise.all(
-          cachesToDelete.map((cacheToDelete) => {
-            return caches.delete(cacheToDelete);
-          })
-        );
-      })
-      .then(() => self.clients.claim())
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== PRECACHE && key !== RUNTIME) {
+            console.log("Removing old cache", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
   );
+
+  self.clients.claim();
 });
 
+// fetch
 self.addEventListener("fetch", (event) => {
-if(event.request.url.includes('/api/' && event.request.method === "GET")){
-    event.respondWith(
-      caches.match(event.request).then((cachedResponse) => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
+  if (event.request.url.includes("/api/" && event.request.method === "GET")) {
+    console.log("Service Worker fetching data....", event.request.url);
 
-        return caches.open(RUNTIME).then((cache) => {
-          return fetch(event.request).then((response) => {
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
+    //if request made to API, clone response and store in cache
+    event.respondWith(
+      caches.open(RUNTIME).then((cache) => {
+        return fetch(event.request)
+          .then((response) => {
+            if (response.status === 200) {
+              cache.put(event.request, response.clone());
+            }
+            return response;
+          })
+          .catch((err) => {
+            console.log(err);
+            return cache.match(event.request);
           });
-        });
       })
     );
+    return;
   }
+
+  //if request is not for the API, send cache data
+  event.respondWith(
+    caches.open(PRECACHE).then((cache) => {
+      return cache.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      });
+    })
+  );
 });
